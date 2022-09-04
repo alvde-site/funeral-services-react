3a29997a5da0053e7d3341988cdcc43e40ce907c
Aleksandr Demidenko <sashatro@mail.ru>
2022-09-04 11:19:30 +0300

    Добавить компоненты в Main

--- src/components/App/Main/Main.js
+++ src/components/App/Main/Main.js
@@ -1,7 +1,29 @@
+import Advertisement from "./Advertisement/Advertisement";
+import Competence from "./Competence/Competence";
+import Services from "./Services/Services";
+import Advantages from "./Advantages/Advantages";
+import Benefits from "./Benefits/Benefits";
+import Portfolio from "./Portfolio/Portfolio";
+import Working from "./Working/Working";
+import Faq from "./Faq/Faq";
+import About from "./About/About";
+import Conditions from "./Conditions/Conditions";
+import Location from "./Location/Location";
+
 function Main() {
   return (
     <main className="content">
-
+      <Advertisement />
+      <Competence />
+      <Services />
+      <Advantages />
+      <Benefits />
+      <Portfolio />
+      <Working />
+      <Faq />
+      <About />
+      <Conditions />
+      <Location />
     </main>
   );
 }
---
+++ src/components/App/PopupWithConfirmation/PopupWithConfirmation.js
@@ -0,0 +1,5 @@
+function PopupWithConfirmation() {
+  return <div className="popup popup_handle_remove-confirm"></div>;
+}
+
+export default PopupWithConfirmation;
