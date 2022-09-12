3a271f3799e1ae46c96178a92d72659ac5c357ff
Aleksandr Demidenko <sashatro@mail.ru>
2022-09-12 18:22:32 +0300

    Добавить разметку для advertisement

--- src/components/App/Main/Advertisement/Advertisement.js
+++ src/components/App/Main/Advertisement/Advertisement.js
@@ -1,5 +1,29 @@
-function Advertisement () {
-  return (<section className="advertisement"></section>)
+function Advertisement() {
+  function handleOpenFeedback() {
+    console.log("Открыть форму")
+  }
+
+  return (
+    <section className="advertisement">
+      <div className="advertisement__content">
+        <h1 className="advertisement__title">
+          Изготовление и установка памятников в Могилеве
+        </h1>
+        <p className="advertisement__subtitle">За 7 дней от 250 BYN</p>
+        <div className="advertisement__info">
+          <button
+            className="advertisement__feedback"
+            onClick={handleOpenFeedback}
+          >
+            Заказать
+          </button>
+          <h2 className="advertisemet__info-title">
+            Изделия из камня любой сложности
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Advertisement;
