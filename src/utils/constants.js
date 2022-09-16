ae3f194413904410e3de182d0f59da129406c1f5
Aleksandr Demidenko <sashatro@mail.ru>
2022-09-16 10:46:29 +0300

    Добавить новые адреса картинки

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,12 +15,12 @@ export const formData = {
 }
 export const portfolioImages = [
   {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
-  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/749e60814c1dff301415f750c68aa704-rimg-w3800-h2544-gmir.jpg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/monument-cross-cemetery-grave-graveyard-rip-portugal-ornaments-evora-1208580.jpg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/monument-statue-cemetery-grave-sculpture-memorial-angel-graveyard-art-rip-portugal-ornaments-evora-1208379.jpg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/flower-monument-statue-cemetery-grave-sculpture-graveyard-art-temple-rip-portugal-ornaments-evora-1168605.jpg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/xxxl.jpeg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/img_0644-2.jpg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/b5b1f17ff9e235bd058547b6c4279875.jpg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/bc5b85e6cb7f3413aa6f250a9bcf2c18.jpg"},
+  {src: "https://0622203.demo1.siteimperium.com/images/2021/04/28/xxxl.jpeg"},
 ]
