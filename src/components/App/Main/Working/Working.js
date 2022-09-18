a1c4834e3aa99a4dd11df34255b7497e65350fbe
Aleksandr Demidenko <sashatro@mail.ru>
2022-09-18 13:06:59 +0300

    Add responsive working

--- src/components/App/Main/Working/Working.js
+++ src/components/App/Main/Working/Working.js
@@ -1,33 +1,51 @@
-function Working () {
-  return (<section className="working">
-    <h1 className="working__title">Как мы работаем</h1>
-    <ul className="working__content">
-      <li className="working__item">
-        <img src="https://0622203.demo1.siteimperium.com/images/2021/04/28/counseling-1.png" alt="Заявка"></img>
-        <h3 className="working__item-title">ШАГ 1</h3>
-        <p className="working__item-subtitle">Отправьте вашу заявку</p>
-        <p className="working__item-desc">Выезд специалиста на замеры при необходимости</p>
-      </li>
-      <li className="working__item">
-        <img src="https://0622203.demo1.siteimperium.com/images/2021/04/28/cemetery-1.png" alt="Заявка"></img>
-        <h3 className="working__item-title">ШАГ 2</h3>
-        <p className="working__item-subtitle">Выбор памятника</p>
-        <p className="working__item-desc">Заключение договора. Внесение предоплаты</p>
-      </li>
-      <li className="working__item">
-        <img src="https://0622203.demo1.siteimperium.com/images/2021/04/28/church-2-1.png" alt="Заявка"></img>
-        <h3 className="working__item-title">ШАГ 3</h3>
-        <p className="working__item-subtitle">Изготовление</p>
-        <p className="working__item-desc">Монтаж в оговоренные сроки</p>
-      </li>
-      <li className="working__item">
-        <img src="https://0622203.demo1.siteimperium.com/images/2021/04/28/angel-1.png" alt="Заявка"></img>
-        <h3 className="working__item-title">ШАГ 4</h3>
-        <p className="working__item-subtitle">Прием изделия</p>
-        <p className="working__item-desc">Подписание акта и гарантии</p>
-      </li>
-    </ul>
-  </section>)
+function Working() {
+  return (
+    <section className="working">
+      <h1 className="working__title">Как мы работаем</h1>
+      <ul className="working__content">
+        <li className="working__item">
+          <img
+            src="https://0622203.demo1.siteimperium.com/images/2021/04/28/counseling-1.png"
+            alt="Заявка"
+          ></img>
+          <h3 className="working__item-title">ШАГ 1</h3>
+          <p className="working__item-subtitle">Отправьте вашу заявку</p>
+          <p className="working__item-desc">
+            Выезд специалиста на замеры при необходимости
+          </p>
+        </li>
+        <li className="working__item">
+          <img
+            src="https://0622203.demo1.siteimperium.com/images/2021/04/28/cemetery-1.png"
+            alt="Заявка"
+          ></img>
+          <h3 className="working__item-title">ШАГ 2</h3>
+          <p className="working__item-subtitle">Выбор памятника</p>
+          <p className="working__item-desc">
+            Заключение договора. Внесение предоплаты
+          </p>
+        </li>
+        <li className="working__item">
+          <img
+            src="https://0622203.demo1.siteimperium.com/images/2021/04/28/church-2-1.png"
+            alt="Заявка"
+          ></img>
+          <h3 className="working__item-title">ШАГ 3</h3>
+          <p className="working__item-subtitle">Изготовление</p>
+          <p className="working__item-desc">Монтаж в оговоренные сроки</p>
+        </li>
+        <li className="working__item">
+          <img
+            src="https://0622203.demo1.siteimperium.com/images/2021/04/28/angel-1.png"
+            alt="Заявка"
+          ></img>
+          <h3 className="working__item-title">ШАГ 4</h3>
+          <p className="working__item-subtitle">Прием изделия</p>
+          <p className="working__item-desc">Подписание акта и гарантии</p>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default Working;
