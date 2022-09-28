4d169e50385515b004aeee33403898a81ced5757
Aleksandr Demidenko <sashatro@mail.ru>
2022-09-28 20:19:29 +0300

    Добавить Link в footer.js

--- src/components/App/Footer/Footer.js
+++ src/components/App/Footer/Footer.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <footer className="footer">
-
+      <Link to="/signin" style={{color: "white", fontSize: "25px"}}>Войти</Link>
     </footer>
   );
 }
