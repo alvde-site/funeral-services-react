eab8f2638632bd56fda7c8d6710484d4dde1144a
Aleksandr Demidenko <sashatro@mail.ru>
2022-10-02 08:45:49 +0300

    Добавить Conditions.js

--- src/components/App/Main/Conditions/Conditions.js
+++ src/components/App/Main/Conditions/Conditions.js
@@ -1,5 +1,25 @@
-function Conditions () {
-  return (<section className="conditions"></section>)
+function Conditions() {
+  return (
+    <section className="conditions">
+      <div className="conditions__content">
+        <h2 className="conditions__title">
+          Сохраните память о ваших близких...
+        </h2>
+        <p className="conditions__subtitle">
+          Обратившись к нам, Вы получите качественное изделие, по доступной цене
+        </p>
+        <ul className="conditions__list">
+          <li className="conditions__list-item">
+            При изготовлении памятника с установкой скидка на установку 10%
+          </li>
+          <li className="conditions__list-item">
+            Бесплатное хранения памятника до 1 года
+          </li>
+          <li className="conditions__list-item">Рассрочка до 6 месяцев</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Conditions;
