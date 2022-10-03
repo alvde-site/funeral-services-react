a3641656e3762328d14984708fbd71a24ef691a3
Aleksandr Demidenko <sashatro@mail.ru>
2022-10-03 13:36:34 +0300

    feat: Add footer.css

--- src/components/App/Footer/Footer.js
+++ src/components/App/Footer/Footer.js
@@ -3,69 +3,75 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__content"><ul className="footer__contacts">
-        <li className="footer__contacts-item">
-          <p className="footer__contacts-hours">Пн-пт с 00:00 до 00:00</p>
-        </li>
-        <li className="footer__contacts-item">
-          <a
-            href="mailto:testform@sitepro.by"
-            className="footer__contacts-email"
-          >
-            alvde@yandex.ru
-          </a>
-        </li>
-        <li className="footer__contacts-item">
-          <a href="tel:+375000000000" className="footer__contacts-tel">
-            +375(00) 000-00-00
-          </a>
-        </li>
-      </ul>
-      <nav>
-        <ul className="contacts__links">
-          <li>
-            <a href="/#services" className="contacts__link">
-              Каталог
+      <div className="footer__content">
+        <ul className="footer__contacts">
+          <li className="footer__item">
+            <div className="footer__logo" src="" alt=""></div>
+          </li>
+          <li className="footer__item">
+            <p className="footer__hours">Пн-пт с 00:00 до 00:00</p>
+          </li>
+          <li className="footer__item">
+            <a
+              href="mailto:testform@sitepro.by"
+              className="footer__email"
+            >
+              alvde@yandex.ru
             </a>
           </li>
-          <li>
-            <a href="/#advantages" className="contacts__link">
-              Преимущества
+          <li className="footer__item">
+            <a href="tel:+375000000000" className="footer__tel">
+              +375(00) 000-00-00
             </a>
-          </li>
-          <li>
-            <a href="/#portfolio" className="contacts__link">
-              Портфолио
-            </a>
-          </li>
-          <li>
-            <a href="/#working" className="contacts__link">
-              Этапы работы
-            </a>
-          </li>
-          <li>
-            <a href="/#faq" className="contacts__link">
-              Вопрос-ответ
-            </a>
-          </li>
-          <li>
-            <a href="#id" className="contacts__link">
-              О компании
-            </a>
-          </li>
-          <li>
-            <a href="#id" className="contacts__link">
-              Контакты
-            </a>
-          </li>
-          <li>
-            <Link to="/signin" className="contacts__link">
-              Войти
-            </Link>
           </li>
         </ul>
-      </nav></div>
-      <p class="footer__copyright">© 2020. Демиденко Александр</p>
+        <nav>
+          <h3 className="footer__links-title">Навигация</h3>
+          <ul className="footer__links">
+            <li>
+              <a href="/#services" className="footer__link">
+                Каталог
+              </a>
+            </li>
+            <li>
+              <a href="/#advantages" className="footer__link">
+                Преимущества
+              </a>
+            </li>
+            <li>
+              <a href="/#portfolio" className="footer__link">
+                Портфолио
+              </a>
+            </li>
+            <li>
+              <a href="/#working" className="footer__link">
+                Этапы работы
+              </a>
+            </li>
+            <li>
+              <a href="/#faq" className="footer__link">
+                Вопрос-ответ
+              </a>
+            </li>
+            <li>
+              <a href="#id" className="footer__link">
+                О компании
+              </a>
+            </li>
+            <li>
+              <a href="#id" className="footer__link">
+                Контакты
+              </a>
+            </li>
+            <li>
+              <Link to="/signin" className="footer__link">
+                Войти
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <a href="https://github.com/alvde-site" target="_blank" rel="noreferrer" className="footer__copyright">© 2022. Демиденко Александр</a>
     </footer>
   );
 }
