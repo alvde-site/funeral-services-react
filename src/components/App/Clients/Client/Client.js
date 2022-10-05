93a9b52b4e9c380b03f49d340cc5a15bdee48906
Aleksandr Demidenko <sashatro@mail.ru>
2022-10-05 10:51:20 +0300

    feat: Add adaptive Client.js

--- src/components/App/Clients/Client/Client.js
+++ src/components/App/Clients/Client/Client.js
@@ -12,18 +12,18 @@ function Client({client}) {
         <p className="clients__data-item">{client.phone}</p>
         <p className="clients__data-item clients__data-item_type_new">{client.status}</p>
         <button
+          className={`clients__edit-button ${isClientOpen ? "clienst__more-button_active": ""}`}
+          area-label="Комментарии к клиенту"
+          type="button"
+        >
+        </button>
+        <button
           className={`clients__more-button ${isClientOpen ? "clients__more-button_active": ""}`}
           area-label="Редактировать"
           type="button"
           onClick={handleClick}
         >
           &#10140;
-        </button>
-        <button
-          className={`clients__edit-button ${isClientOpen ? "clienst__more-button_active": ""}`}
-          area-label="Комментарии к клиенту"
-          type="button"
-        >
         </button>
       </div>
       <p className={`clients__description ${isClientOpen ? "clients__description_active": ""}`}>{client.description}</p>
